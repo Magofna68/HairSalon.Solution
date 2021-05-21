5ff26f85fe455354dced0beb3163102f6e450a73
Brandon-Magofna <magofna68@gmail.com>
2021-05-21 10:15:47 -0700

    build out views/client/index  along with corresponding controller actions

--- HairSalon/Controllers/ClientController.cs
+++ HairSalon/Controllers/ClientController.cs
@@ -1,10 +1,25 @@
-// using System.Collections.Generic;
-// using Microsoft.AspNetCore.Mvc;
-// using HairSalon.Models;
-// using System.Linq;
-// using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
-// namespace HairSalon.Controllers
-// {
-//   public class 
-// }
+namespace HairSalon.Controllers
+{
+  public class ClientController : Controller
+  {
+    private readonly HairSalonContext _db;
+
+    public ClientController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Client> model = _db.Clients.Include(client => client.Stylist).ToList();
+      return View(model);
+    }
+  }
+}
