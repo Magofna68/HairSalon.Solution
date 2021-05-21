b68731ec288d6ee2487869d24890cc2cb4ac71fb
Brandon-Magofna <magofna68@gmail.com>
2021-05-21 08:48:45 -0700

    build out models/stylist

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -1,7 +1,16 @@
-namespace ProjectName
-{
-  public class ClassName
-  {
+using System.Collections.Generic;
 
+namespace HairSalon.Models
+{
+  public class Stylist
+  {
+    public Stylist()
+    {
+      this.Client = new HashSet<Client>();
+    }
+
+    public int StylistId { get; set; }
+    public string Name { get; set; }
+    public virtual ICollection<Client> Client { get; set; }
   }
 }
